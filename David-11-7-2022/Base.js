7a1a1b3a0ff814a96bef231250e7e6ee01d24f49
ProDavidInAction <thegruffalosgames123@gmail.com>
2022-07-14 20:30:53 +0200

    js and style point fix

--- David-11-7-2022/Base.js
+++ David-11-7-2022/Base.js
@@ -39,18 +39,22 @@ function getTime()
 	if(time > 6 && time < 11 || time == 11)
 	{
 		document.getElementById("wake_up_image").src="https://static.toiimg.com/photo/45773211.cms";
+		document.getElementById("initial_statement").innerHTML = "Good Morning!";
 	}
 	else if(time > 11 && time < 18 || time == 18)
 	{
 		document.getElementById("wake_up_image").src="https://media.gettyimages.com/videos/beautiful-summer-sun-time-lapse-video-id638668442?s=640x640";
+		document.getElementById("initial_statement").innerHTML = "Good Day!";
 	}
 	else if(time > 18 && time < 20 || time == 20)
 	{
 		document.getElementById("wake_up_image").src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/blue-sky-above-the-setting-sun-lyle-crump.jpg";
+		document.getElementById("initial_statement").innerHTML = "Good Afternoon!";
 	}
 	else
 	{
 		document.getElementById("wake_up_image").src="https://images.unsplash.com/photo-1499578124509-1611b77778c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGx1bmF8ZW58MHx8MHx8&w=1000&q=80";
+		document.getElementById("initial_statement").innerHTML = "You shoul be asleep!";
 	}
 }
 
