b01d511377232921097c3f37278a3ec5f34657c1
ProDavidInAction <thegruffalosgames123@gmail.com>
2022-07-14 17:27:54 +0200

    js change login message

--- David-11-7-2022/Base.js
+++ David-11-7-2022/Base.js
@@ -1,13 +1,11 @@
-
-
 function login()
 {
 	var name = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 	if(name == "BonzyBuddy" && password == "Calvo")
 	{
-		window.alert("Login Correct");
 		window.location.href="./logedin.html";
+    window.alert("Login Correct");
 		window.alert("This is your home page, you are free to explore");
 	}
 	else
@@ -26,36 +24,3 @@ function NotLoggedInFault()
 	window.alert("Log in or sign up before you can access this page");
 }
 
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function checkCookie() {
-  let user = getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-    user = prompt("Please enter your name:", "");
-    if (user != "" && user != null) {
-      setCookie("username", user, 365);
-    }
-  }
-}
