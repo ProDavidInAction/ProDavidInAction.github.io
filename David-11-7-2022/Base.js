abc0517314ab8c0add64dd76d1a1ed5bb1187f9a
ProDavidInAction <thegruffalosgames123@gmail.com>
2022-07-14 17:37:13 +0200

    login system showing message when loaded

--- David-11-7-2022/Base.js
+++ David-11-7-2022/Base.js
@@ -2,6 +2,11 @@ function login()
 {
 	var name = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
+
+  let name = ["BonzyBuddy", "ProDavid"];
+  let password = ["Calvo", "123"];
+
+
 	if(name == "BonzyBuddy" && password == "Calvo")
 	{
 		window.location.href="./logedin.html";
